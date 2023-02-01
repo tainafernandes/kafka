b4a7f7f1f9e5ff753368362f3e368a736617905a
tainafernandes <tainafefe@gmail.com>
2023-02-01 13:47:09 -0300

    first commit

---
+++ str-producer/src/main/java/io/github/tainafernandes/strproducer/StrProducerApplication.java
@@ -0,0 +1,13 @@
+package io.github.tainafernandes.strproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrProducerApplication.class, args);
+	}
+
+}
---
+++ str-producer/src/test/java/io/github/tainafernandes/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tainafernandes.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
