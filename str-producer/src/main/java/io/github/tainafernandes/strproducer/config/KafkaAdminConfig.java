4be18d86d759ee125d19e1348931b125ba5c9344
tainafernandes <tainafefe@gmail.com>
2023-02-01 14:17:11 -0300

    Configuration KafkaAdmin

---
+++ str-producer/src/main/java/io/github/tainafernandes/strproducer/config/KafkaAdminConfig.java
@@ -0,0 +1,30 @@
+package io.github.tainafernandes.strproducer.config;
+
+import java.util.HashMap;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+@Configuration
+@RequiredArgsConstructor
+public class KafkaAdminConfig {
+
+    public final KafkaProperties properties;
+    @Bean
+    public KafkaAdmin kafkaAdmin(){
+        var configs = new HashMap<String, Object>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getBootstrapServers());
+        return new KafkaAdmin(configs);
+    }
+    @Bean
+    public KafkaAdmin.NewTopics topics(){
+        return new  KafkaAdmin.NewTopics(
+                TopicBuilder.name("str-topic").partitions(2).replicas(1).build()
+        );
+    }
+}
